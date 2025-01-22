c395b8126c6e51c332cdade2e103e849d2681514
wesley-stecker <Spaceboy623@gmail.com>
2025-01-22 16:16:50 -0700

    highscore

--- creeps/main.gd
+++ creeps/main.gd
@@ -1,11 +1,12 @@
 extends Node
 
 @export var mob_scene: PackedScene
-var score
+var score = 0
+var highscore = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,17 +17,28 @@ func _process(delta: float) -> void:
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
-
+func updatehighscorefunc (score):
+	if score >= highscore:
+		highscore = score
+	$HUD.update_highscore(highscore)
 
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
+	updatehighscorefunc(score)
+	
+	
+
 
 
 func _on_start_timer_timeout() -> void:
